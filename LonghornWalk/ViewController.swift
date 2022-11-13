c81204dc3ab1a22bfcd42d77890344693a609e40
Team10 <mtg2382@utexas.edu>
2022-11-13 11:39:16 -0600

    test explaining changes made

--- LonghornWalk/ViewController.swift
+++ LonghornWalk/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Test for M for github")
+        print("Look for number")
     }
 
 
