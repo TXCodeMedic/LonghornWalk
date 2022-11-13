2a66f27c29862bce8ba3c09a3d94c9f16f95ad55
Team10 <mtg2382@utexas.edu>
2022-11-13 12:01:58 -0600

    Added VCs for each screen currently in development - Matthew

---
+++ LonghornWalk/LoginAndRegistrationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginAndRegistrationViewController.swift
+//  LonghornWalk
+//
+//  Created by Matthew Galvez on 11/13/22.
+//
+
+import UIKit
+
+class LoginAndRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
