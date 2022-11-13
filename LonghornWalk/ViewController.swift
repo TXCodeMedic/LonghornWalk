904599630911f30c998aaa5b9549cab678865cb3
Team10 <mtg2382@utexas.edu>
2022-11-13 11:34:50 -0600

    Initial Commit

---
+++ LonghornWalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LonghornWalk
+//
+//  Created by Matthew Galvez on 11/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
