82aa18c1d0ace5a7251bded863559a8a10ceee96
cyberkatrina <katrinaaliashkevich@utexas.edu>
2022-11-17 20:53:47 -0600

    profile photo progress

--- LonghornWalk/UserProfileViewController.swift
+++ LonghornWalk/UserProfileViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import AVFoundation
 
-class UserProfileViewController: UIViewController {
-
+class UserProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let picker = UIImagePickerController()
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // request authorization from the user for our app to send notifications
+        picker.delegate = self
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
             granted, error in
             if granted {
@@ -58,6 +64,7 @@ class UserProfileViewController: UIViewController {
                     if accepted == true {
                         let notification_accepted = UNMutableNotificationContent()
                         notification_accepted.title = "Longhorn Walk:"
+                        
                         notification_accepted.subtitle = "Congratulations!"
                         notification_accepted.body = "\(textField!.text!) accepted your friend request :)"
                         notification_accepted.sound = UNNotificationSound.default
@@ -87,4 +94,80 @@ class UserProfileViewController: UIViewController {
         self.present(friendInput, animated: true, completion: nil)
         // ASK BILL how best to alternate between request accpeted and denied notifications
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let chosenImage = info[.originalImage] as! UIImage
+        profilePhoto.contentMode = .scaleAspectFit
+        profilePhoto.image = chosenImage
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func changePhotoPressed(_ sender: Any) {
+        let controller = UIAlertController(
+            title: "Choose your Profile Photo",
+            message: "Take from:",
+            preferredStyle: .actionSheet)
+        // if regular cheese option is selected, make that the cheese type of the pizza object
+        let Camera = UIAlertAction(
+            title: "Camera",
+            style: .default,
+            handler: {_ in
+                if UIImagePickerController.availableCaptureModes(for: .rear) != nil {
+                    // use the rear camera
+                    switch AVCaptureDevice.authorizationStatus(for: .video) {
+                    case .notDetermined:
+                        // we don't know
+                        AVCaptureDevice.requestAccess(for: .video) {
+                            accessGranted in
+                            guard accessGranted == true else { return }
+                        }
+                    case .authorized:
+                        // we have permission already
+                        break
+                    default:
+                        // we know we don't have access
+                        print("Access denied")
+                        return
+                    }
+                self.picker.allowsEditing = false
+                self.picker.sourceType = .camera
+                self.picker.cameraCaptureMode = .photo
+                self.present(self.picker, animated: true)
+                
+                } else {
+                    // no rear camera is available
+                    
+                    let alertVC = UIAlertController(
+                        title: "No camera",
+                        message: "Sorry, this device has no rear camera",
+                        preferredStyle: .alert)
+                    let okAction = UIAlertAction(
+                        title: "OK",
+                        style: .default)
+                    alertVC.addAction(okAction)
+                    self.present(alertVC,animated:true)
+                }
+            }
+        )
+        
+            controller.addAction(Camera)
+        // if no cheese option is selected, make that the cheese type of the pizza object
+        let Library = UIAlertAction(
+            title: "Photo Library",
+            style: .default,
+            handler: {_ in
+                self.picker.allowsEditing = false
+                self.picker.sourceType = .photoLibrary
+                self.present(self.picker, animated: true)
+                
+            })
+        controller.addAction(Library)
+        
+        present(controller, animated: true)
+    }
+    
 }
