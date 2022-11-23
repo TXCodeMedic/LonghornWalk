d3ba56cbbdbbc2dbba5ca8005625517642e9afd4
cyberkatrina <katrinaaliashkevich@utexas.edu>
2022-11-23 07:13:12 -0600

    user class added

---
+++ User.swift
@@ -0,0 +1,37 @@
+//
+//  User.swift
+//  LonghornWalk
+//
+//  Created by Katrina Aliashkevich on 11/22/22.
+//
+
+import UIKit
+
+class User {
+
+    public var username: String
+    public var password: String
+    public var points: Int
+    public var displayName: String
+    // YYYY-MM-DD format for join date
+    public var joinDate: String
+    public var friendsList: [String]
+    public var locationsVisited: [String]
+    // file path to the image file stored in String format? not sure yet
+    public var profilePic: String
+    // store the setting preferences of the user somehow?
+    public var settingPreferences: String
+    
+
+    init(username: String, password: String, joinDate: String) {
+        self.username = username
+        self.password = password
+        self.joinDate = joinDate
+        self.points = 0
+        self.displayName = ""
+        self.friendsList = []
+        self.locationsVisited = []
+        self.profilePic = ""
+        self.settingPreferences = ""
+    }
+}
